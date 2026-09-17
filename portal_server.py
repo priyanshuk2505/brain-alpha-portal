870eaae9e0d24e8d1ea8a4ed439b30b34b2702f7
Priyanshu Kumar <priyanshukumar@Priyanshus-MacBook-Air.local>
2026-09-18 01:20:02 +0530

    Disable browser cache and remove datalist element for pure text input box

--- portal_server.py
+++ portal_server.py
@@ -936,6 +936,13 @@ for _slot_id in range(MAX_CONCURRENT_SIMS):
 # -----------------------------------------------------------------------------
 # REST API Endpoints
 # -----------------------------------------------------------------------------
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 @app.route("/")
 def index():
     return render_template("index.html")
